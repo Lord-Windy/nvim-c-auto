10d4593be9e9ae5abe77db964f9c80a8f60bf2a4
Samuel Brown <sam@samuelbrown.me>
2017-08-11 11:51:50 +1000

    Made changes

--- rplugin/python3/expansions.py
+++ rplugin/python3/expansions.py
@@ -5,7 +5,7 @@ class Main(object):
     def __init__(self, vim):
         self.vim = vim
 
-    @neovim.function('DoItPython')
+    @neovim.function('samfor')
     def doItPython(self, args):
-        self.vim.command('echo "hello from DoItPython"')
+        self.vim.command('echo "Sams thing this time"')
 
